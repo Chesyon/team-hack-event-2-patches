254280524700043eef0ab5fe9620f54e74a0bd6c
StolenBurrito <tylermercier9@gmail.com>
2025-09-21 20:08:47 -0400

    uhhhh i added no quicksaves yeah

--- src/no_quicksaves.s
+++ src/no_quicksaves.s
@@ -1,1 +1,12 @@
 
+HookQuickSave:
+		stmdb r13!,{r0,r1,r2,r3}
+		mov r0,#0
+		mov r1,#78
+		mov r2,#62
+		bl LoadScriptVariableAtIndex
+		cmp r0,#0
+		ldmia r13!,{r0,r1,r2,r3}
+		bne UNK_ANON_HOOK ; 0x0238897C
+		ldr r0,[r1, #+0x8]
+		b EndHookQuickSave
