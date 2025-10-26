9d4b618f473e5c9812e975a46684bf2d43997c51
StolenBurrito <tylermercier9@gmail.com>
2025-10-26 18:50:03 -0400

    snuff out & unified beatdown
    because i hate happylappy

---
+++ external_asm/move_effects/snuff_out.asm
@@ -0,0 +1,127 @@
+; r4 = Target
+; r5 = Garbage Data
+; r6 = Move ID
+; r7 = ID of the item that called the move (for orbs. If the move is not for an orb, ignore this)
+; r8 = Move Data
+; r9 = User
+; 10 = Unknown (usually 0 or 1, no clue on the context)
+
+; NOTE: registers r4 through r9, r11 and r13/sp must remain unchanged by the time the code reaches `b MoveJumpAddress`
+; if you need to use these registers, use push/pop commands
+; push stores its current value and pop returns it to the value that was stored
+
+.relativeinclude on
+.nds
+.arm
+
+.include "lib/stdlib_us.asm"
+.include "lib/dunlib_us.asm"
+.definelabel MoveStartAddress, 0x2330134
+.definelabel MoveJumpAddress, 0x23326CC
+.definelabel TryInflictTerrifiedStatus, 0x2314E60
+.definelabel DungeonRandOutcome, 0x22EAB20
+.definelabel DungeonRandInt, 0x22EAA98
+.definelabel SoundproofAbilityID, 0x3C ; 60
+.definelabel SoundproofStrID, 0xEB9 ; 3769
+.definelabel DUNGEON_PTR, 0x02353538
+.definelabel TryInflictCringeStatus, 0x23143E8
+.definelabel LowerOffensiveStat, 0x23135FC
+.definelabel LowerSpeed, 0x2314954
+.definelabel LowerDefensiveStat, 0x2313814
+.definelabel TryWarp, 0x2320D08
+.definelabel EntityIsValid, 0x22E0354
+.definelabel TryInflictConfusedStatus, 0x2314F38
+.definelabel EndBurnClassStatus, 0x23061A8
+.definelabel CalcDamageFixed, 0x230D240
+.definelabel AttackUp, 0x231399C
+.definelabel LogMessageByIdWithPopupCheckUserTarget, 0x234B350
+
+; snuff out heals the targets burn, deals damage to the target, and then boosts the users attack by 1 stage
+
+.definelabel MaxSize, 0x2598
+
+.create "./code_out.bin", 0x2330134
+    .org MoveStartAddress
+    .area MaxSize
+
+    	sub sp,sp,14h
+
+	mov  r11, r11
+
+	; does the target have burn?
+
+	ldr  r0, [r4, #0xb4]
+	ldrb r1, [r0, #0xbf]
+	cmp  r1, #1
+	bne  NotBurned
+
+	; is the target an ally? cut damage in half if so
+
+	ldrb  r1, [r0, #0x6]
+	cmp   r1, #1
+	moveq r3, #256
+	beq   IsEnemy
+
+	push  r5 - r7
+
+	ldrb  r5, [r0, #0xd5]
+	ldrb  r6, [r0, #0xd6]
+	mov   r1, #5
+	strb  r1, [r0, #0xd6]
+	mov   r1, #9
+	strb  r1, [r0, #0xd5]
+
+	mov  r7, r0
+
+	mov  r0, r9
+	mov  r1, r4
+	mov  r2, r8
+	mov  r3, #128
+	bl   DealDamage
+
+	strb r5, [r7, #0xd5]
+	strb r6, [r7, #0xd6]
+	
+	pop  r5-r7
+	b    RaiseAttack
+
+	IsEnemy:
+
+	mov  r0, r9
+	mov  r1, r4
+	mov  r2, r8
+	bl   DealDamage
+
+	RaiseAttack:
+
+	mov  r0, r9
+	mov  r1, r9
+	mov  r2, #0
+	mov  r3, #1
+	bl   AttackUp	
+
+	mov  r0, r4
+	bl   EntityIsValid
+	cmp  r0, #1
+	bne  return
+
+	mov  r0, r9
+	mov  r1, r4
+	bl   EndBurnClassStatus
+
+	b    return
+
+	NotBurned:
+
+	mov  r0, r9
+	mov  r1, r4
+	ldr  r2, =#3892
+	bl   LogMessageByIdWithPopupCheckUserTarget
+
+    return:
+        add  sp,sp,14h
+        b    MoveJumpAddress
+	
+        .pool
+    .endarea
+.close
---
+++ external_asm/move_effects/unified_beatdown.asm
@@ -0,0 +1,96 @@
+; r4 = Target
+; r5 = Garbage Data
+; r6 = Move ID
+; r7 = ID of the item that called the move (for orbs. If the move is not for an orb, ignore this)
+; r8 = Move Data
+; r9 = User
+; 10 = Unknown (usually 0 or 1, no clue on the context)
+
+; NOTE: registers r4 through r9, r11 and r13/sp must remain unchanged by the time the code reaches `b MoveJumpAddress`
+; if you need to use these registers, use push/pop commands
+; push stores its current value and pop returns it to the value that was stored
+
+.relativeinclude on
+.nds
+.arm
+
+.include "lib/stdlib_us.asm"
+.include "lib/dunlib_us.asm"
+.definelabel MoveStartAddress, 0x2330134
+.definelabel MoveJumpAddress, 0x23326CC
+.definelabel TryInflictTerrifiedStatus, 0x2314E60
+.definelabel DungeonRandOutcome, 0x22EAB20
+.definelabel DungeonRandInt, 0x22EAA98
+.definelabel SoundproofAbilityID, 0x3C ; 60
+.definelabel SoundproofStrID, 0xEB9 ; 3769
+.definelabel DUNGEON_PTR, 0x02353538
+.definelabel TryInflictCringeStatus, 0x23143E8
+.definelabel LowerOffensiveStat, 0x23135FC
+.definelabel LowerSpeed, 0x2314954
+.definelabel LowerDefensiveStat, 0x2313814
+.definelabel TryWarp, 0x2320D08
+.definelabel EntityIsValid, 0x22E0354
+.definelabel TryInflictConfusedStatus, 0x2314F38
+
+; unified beatdown is a single strike in solo form, but 3 strikes in schooling form. for balancing reasons, the 3 strikes deal 40% of the original damage.
+
+.definelabel MaxSize, 0x2598
+
+.create "./code_out.bin", 0x2330134
+    .org MoveStartAddress
+    .area MaxSize
+
+    	sub sp,sp,#4
+
+	ldr  r0, [r9, #0xb4]
+	ldrh r1, [r0, #0x4] ; note that im using apparent ID instead of ID. this is because apparent ID is what should change for form changing mons like wishiwashi
+	ldr  r2, =539 ; note: change this to 0x219 later. its set to charmanders ID for testing
+	cmp  r1, r2
+	beq  TripleStrike
+
+	mov  r0,r9
+	mov  r1,r4
+	mov  r2,r8
+	mov  r3,#0x100
+	bl   DealDamage
+
+	b    return
+
+	TripleStrike:
+
+	mov  r0,r9
+	mov  r1,r4
+	mov  r2,r8
+	mov  r3,#0x66
+	bl   DealDamage
+
+	mov  r0, r4
+	bl   EntityIsValid
+	cmp  r0, #1
+	bne  return
+
+	mov  r0,r9
+	mov  r1,r4
+	mov  r2,r8
+	mov  r3,#0x66
+	bl   DealDamage
+
+	mov  r0, r4
+	bl   EntityIsValid
+	cmp  r0, #1
+	bne  return
+
+	mov  r0,r4
+	mov  r1,r9
+	mov  r2,r8
+	mov  r3,#0x66
+	bl   DealDamage
+
+
+    return:
+        add  sp,sp,#4
+        b    MoveJumpAddress
+	
+        .pool
+    .endarea
+.close
