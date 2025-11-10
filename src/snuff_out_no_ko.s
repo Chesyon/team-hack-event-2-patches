7a7d6440cdda43bfd61174fe54499b4cfb2109a2
StolenBurrito <tylermercier9@gmail.com>
2025-11-09 20:42:20 -0500

    Create snuff_out_no_ko.s

---
+++ src/snuff_out_no_ko.s
@@ -0,0 +1,16 @@
+CanTheMoveKO:
+
+cmp r0, r1
+moveq r5, #1
+beq Continue
+
+ldr r0, =#413 // snuff out's ID
+cmp r0, r1
+bne Continue
+
+ldr r0, [r7, #0xb4] // r7 is the target, r8 is the user and r4 is the user's monster struct, but nothing holds the target's monster struct????
+ldrb r1, [r0, #0x6]
+cmp r1, #0
+moveq r5, #1
+movne r5, #0
+b Continue
