4007c930a70fd77af23a7f344ca8ad4577b93b71
StolenBurrito <tylermercier9@gmail.com>
2025-11-14 21:05:28 -0500

    Update snuff_out_no_ko.s

--- src/snuff_out_no_ko.s
+++ src/snuff_out_no_ko.s
@@ -6,6 +6,7 @@ beq IDontKnowMayItKO
 
 ldr r0, =#413 // snuff out's ID
 cmp r0, r1
+movne r5, #0
 bne IDontKnowMayItKO
 
 ldr r0, [r7, #0xb4] // r7 is the target, r8 is the user and r4 is the user's monster struct, but nothing holds the target's monster struct????
