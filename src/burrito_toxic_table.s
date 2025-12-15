494c956813d25f7c8db803ba3a475de1cf353577
StolenBurrito <tylermercier9@gmail.com>
2025-12-14 23:42:20 -0500

    Create burrito_toxic_table.s
    uhhhhh toxic damage?

---
+++ src/burrito_toxic_table.s
@@ -0,0 +1,23 @@
+WhichToxicTable:
+  push     r14
+  push     r0-r2
+  mov      r0, #87
+  bl       CheckDungeonId ; found in larvesta_item_check.s
+  cmp      r0, #1
+  beq      WeAreInTheBeam
+  cmp      r3, #0
+  pop      r0-r2
+  moveq    r2, #6
+  movne    r1, #6
+  pop    r15
+
+WeAreInTheBeam:
+
+  ; r3 determines if we came from poison heal or poison damage
+
+  cmp  r3, #0
+  mov  r1, r6, lsl #1
+  ldrsheq  r2, [r0, r1]
+  ldrshne  r1, [r0, r1]
+  pop      r15
+  
