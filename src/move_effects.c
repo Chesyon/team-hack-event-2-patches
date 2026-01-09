52bd2f33192991c579120b0372275d85c01cae43
Caitemis <caitemis@gmail.com>
2026-01-08 22:15:51 -0500

    Update move_effects.c
    Replacing the dummy placeholder with custom Roar of Time stuff. Yay.

--- src/move_effects.c
+++ src/move_effects.c
@@ -1,39 +1,25 @@
 #include <pmdsky.h>
 #include <cot.h>
 
-// Implements the "Body Press" move
-// Based on https://github.com/Adex-8x/EoS-ASM-Effects/blob/main/moves/gen8/body_press.asm
-// Deals damage based on the user's defense instead of attack stat
-static bool MoveBodyPress(struct entity *user, struct entity *target, struct move *move)
-{
-  if (user->type == ENTITY_MONSTER)
-  {
-    struct monster *user_monster = (struct monster *)user->info;
 
-    int old_attack = user_monster->offensive_stats[0];
-    user_monster->offensive_stats[0] = user_monster->defensive_stats[0];
-
-    bool dealt_damage = DealDamage(user, target, move, 0x100, ITEM_NOTHING);
-
-    user_monster->offensive_stats[0] = old_attack;
-    return dealt_damage;
-  }
-  return false;
+//Roar of Time variant that has the user Pounce in the direction it's facing. Put in Roar of Time slot (for forced Recharge) and is set to line of sight.
+static bool MoveRoarOfTime(struct entity* user, struct entity* target, struct move* move) {
+    if (!DealDamage(user, target, move, 0x100, ITEM_NOTHING)) {
+        return false;
+    }
+    TryPounce(user, user, 8);
+    return true;
 }
 
-// Called when using moves. Should return true if a custom effect was applied.
-// This function is only called if the move doesn't fail due to a missing target
 bool CustomApplyMoveEffect(
-    move_effect_input *data, struct entity *user, struct entity *target, struct move *move)
+    move_effect_input* data, struct entity* user, struct entity* target, struct move* move)
 {
-  switch (data->move_id)
-  {
-  case MOVE_SCRATCH:
-    // Replace move 260 (Scratch) with custom Body Press effect
-    data->out_dealt_damage = MoveBodyPress(user, target, move);
-    // Return true to signal that we've handled the effect
-    return true;
-  default:
-    return false;
-  }
+    switch (data->move_id)
+    {
+    case MOVE_ROAR_OF_TIME:
+        data->out_dealt_damage = MoveRoarOfTime(user, target, move);
+        return true;
+    default:
+        return false;
+    }
 }
